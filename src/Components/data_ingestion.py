649410b178b1142fc176e871c605a4c4c820a68e
SaiSrinivas1997 <sai92186@gmail.com>
2023-04-03 17:06:07 +0530

    Data Ingestion

--- src/Components/data_ingestion.py
+++ src/Components/data_ingestion.py
@@ -35,6 +35,6 @@ class DataIngestion:
 
             return(self.ingestion_config.train_data_path, self.ingestion_config.test_data_path)
         except Exception as e:
-            raise CustomException()
+            raise CustomException(e, sys)
         
 
